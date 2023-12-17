27c0db02b7fe0a12bbfc4fa13e680fe3536751f4
kmoroz <01184776@pw.edu.pl>
2023-12-17 16:10:52 +0100

    first

---
+++ src/main/java/bdbt_project/Filharmonia/FilharmoniaApplication.java
@@ -0,0 +1,13 @@
+package bdbt_project.Filharmonia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilharmoniaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilharmoniaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bdbt_project/Filharmonia/FilharmoniaApplicationTests.java
@@ -0,0 +1,13 @@
+package bdbt_project.Filharmonia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilharmoniaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

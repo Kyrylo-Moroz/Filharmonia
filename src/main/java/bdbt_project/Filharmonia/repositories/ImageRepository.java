e6807c296b449d2e595595ea34c880f00110f07d
kmoroz <01184776@pw.edu.pl>
2023-12-24 15:13:05 +0100

    lesson 4

---
+++ src/main/java/bdbt_project/Filharmonia/repositories/ImageRepository.java
@@ -0,0 +1,7 @@
+package bdbt_project.Filharmonia.repositories;
+
+import bdbt_project.Filharmonia.models.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImageRepository extends JpaRepository<Image, Long> {
+}

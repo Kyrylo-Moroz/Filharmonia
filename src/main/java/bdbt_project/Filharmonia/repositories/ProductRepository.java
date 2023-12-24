d7b68bc551a796fbb24ae8de431904c072ba64a7
kmoroz <01184776@pw.edu.pl>
2023-12-24 01:32:16 +0100

    lesson 3

---
+++ src/main/java/bdbt_project/Filharmonia/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package bdbt_project.Filharmonia.repositories;
+
+import bdbt_project.Filharmonia.models.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByTitle(String title);
+}
+
--- src/main/java/bdbt_project/Filharmonia/services/ProductService.java
+++ src/main/java/bdbt_project/Filharmonia/services/ProductService.java
@@ -1,37 +1,35 @@
 package bdbt_project.Filharmonia.services;
 
 import bdbt_project.Filharmonia.models.Product;
+import bdbt_project.Filharmonia.repositories.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@Slf4j
+@RequiredArgsConstructor
 public class ProductService {
-    private List<Product> products = new ArrayList<>();
-    private long ID = 0;
+    private final ProductRepository productRepository;
 
-    {
-        products.add(new Product(++ID,"Ps5","simple",12000,"Warszawa","Ja"));
-        products.add(new Product(++ID,"Iphone 15","simple",100000,"Gdańsk","Piotr"));
+    public List<Product> listProducts(String title) {
+        if (title != null) return productRepository.findByTitle(title);
+        return productRepository.findAll();
     }
 
-    public List<Product> listProducts() {return products; }
-
-    public void saveProduct(Product product){
-        product.setId(++ID);
-        products.add(product);
+    public void saveProduct(Product product) {
+        log.info("Saving new {}", product);
+        productRepository.save(product);
     }
 
     public void deleteProduct(Long id) {
-        products.removeIf(product -> product.getId().equals(id));
+        productRepository.deleteById(id);
     }
 
     public Product getProductById(Long id) {
-        for (Product product : products) {
-            if (product.getId().equals(id)) return product;
-        }
-        return null;
+        return productRepository.findById(id).orElse(null);
     }
-
 }

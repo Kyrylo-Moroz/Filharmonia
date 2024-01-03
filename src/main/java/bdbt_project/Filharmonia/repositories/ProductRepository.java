91371f2efcda8952967bb7fd80d6c7c757f086d9
kmoroz <01184776@pw.edu.pl>
2024-01-03 17:48:16 +0100

    product search has been fixed

--- src/main/java/bdbt_project/Filharmonia/repositories/ProductRepository.java
+++ src/main/java/bdbt_project/Filharmonia/repositories/ProductRepository.java
@@ -7,5 +7,6 @@ import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findByTitle(String title);
+    List<Product> findByTitleContainingIgnoreCase(String title);
 }
 

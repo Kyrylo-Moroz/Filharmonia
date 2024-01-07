f5cdcb2dd00a14e65a4d637a28c8977964711567
Marcin <marcin.lusawa.2003@gmail.com>
2024-01-07 21:33:26 +0100

    error styles, login style, register buttons style, rest is to work about

--- src/main/java/bdbt_project/Filharmonia/controllers/MyErrorController.java
+++ src/main/java/bdbt_project/Filharmonia/controllers/MyErrorController.java
@@ -3,7 +3,6 @@ package bdbt_project.Filharmonia.controllers;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.boot.web.servlet.error.ErrorController;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 

da2bb74c1cb264caa7c01b830236db61442ecfae
Marcin <marcin.lusawa.2003@gmail.com>
2024-01-07 20:08:02 +0100

    error handling works

--- src/main/java/bdbt_project/Filharmonia/controllers/MyErrorController.java
+++ src/main/java/bdbt_project/Filharmonia/controllers/MyErrorController.java
@@ -1,31 +1,31 @@
 package bdbt_project.Filharmonia.controllers;
 
-import lombok.RequiredArgsConstructor;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.http.HttpServletRequest;
 @Controller
-@RequiredArgsConstructor
 public class MyErrorController implements ErrorController {
 
     @RequestMapping("/error")
     public String handleError(HttpServletRequest request) {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
         if (status != null) {
             Integer statusCode = Integer.valueOf(status.toString());
-            if (statusCode == HttpStatus.FORBIDDEN.value()) {
-                return "errors/403";
-            } else if (statusCode == HttpStatus.NOT_FOUND.value()) {
-                return "errors/404";
-            } else if (statusCode == HttpStatus.BAD_REQUEST.value()) {
+
+            if (statusCode == 400) {
                 return "errors/400";
-            } else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+            } else if (statusCode == 403) {
+                return "errors/403";
+            } else if (statusCode == 404) {
+                return "errors/404";
+            } else if (statusCode == 500) {
                 return "errors/500";
-            } else if (statusCode == HttpStatus.GATEWAY_TIMEOUT.value()) {
+            } else if (statusCode == 504) {
                 return "errors/504";
             } else {
                 return "errors/other";
